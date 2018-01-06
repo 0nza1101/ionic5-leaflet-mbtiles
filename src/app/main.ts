31e95c3f868b9e952789bc27e19a026bb8a861f2
Jordan <onza1101@gmail.com>
2018-01-07 00:07:37 +0100

    Added fetch and promise polyfills

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+//Polyfills
+import 'promise-polyfill';
+import 'whatwg-fetch';
+
 platformBrowserDynamic().bootstrapModule(AppModule);
